03b33b2cebf99be7a22ed13ab2b7589c96f42909
Matt Rangel <mattrangel@gmail.com>
2022-10-20 14:58:06 -0700

    footer icons updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,51 @@
 import React from "react";
-import { FaMapMarkedAlt, FaInstagram, FaPhoneSquareAlt } from "react-icons/fa";
+import { FaMapMarkedAlt, FaInstagram, FaPhoneSquareAlt, FaFacebook, FaYelp } from "react-icons/fa";
+import '../App.css'
 
 
 function Footer() {
   
   return (
   <footer rel="stylesheet" href="style.module.css">
-      <div className="footer-container">
+      <div className="footer-container"
+      onMouseOver={'white'} 
+      onMouseOut={'grey'}>
         <div className="row">
           <div className="col social-links">
             <a
               href="https://instagram.com/centralcoast_barbell?igshid=YmMyMTA2M2Y=">
               <FaInstagram
-                size={50}
-                color="white"
+                size={30}
+
+                class="icon"
+              />
+            </a>
+            <a
+              href="https://www.facebook.com/CentralCoastBarbell">
+              <FaFacebook
+                size={30}
+                class="icon"
+              />
+            </a>
+            <a
+              href="https://www.yelp.com/biz/central-coast-barbell-grover-beach?utm_campaign=www_business_share_popup&utm_medium=copy_link&utm_source=(direct)">
+              <FaYelp
+                size={30}
+                class="icon"
               />
             </a>
             <a
               href="https://www.google.com/maps/place/Central+Coast+Barbell/@35.1092097,-120.6184355,21z/data=!4m13!1m7!3m6!1s0x80ec5ead667d5a27:0xdfac6f9659f20cc5!2s1081+Highland+Way+a,+Grover+Beach,+CA+93433!3b1!8m2!3d35.1092097!4d-120.6182988!3m4!1s0x80ec594b9187f103:0x1790f1c03fca0846!8m2!3d35.1092097!4d-120.6182987">
               <FaMapMarkedAlt
-                size={50}
-                color="white"
+                size={30}
+                class="icon"
               />
               </a>
               <a
               href="+18054891618">
               <FaPhoneSquareAlt
-                size={50}
-                color="white"
+                size={30}
+                class="icon"
               />
               </a>
           </div>
