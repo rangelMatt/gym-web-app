dbeed2646e79ecd8c18d909113df043842bb6783
Matt Rangel <mattrangel@gmail.com>
2022-10-27 14:51:36 -0700

    Equipment content added

---
+++ src/components/DayPass.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./Button.module.css";
+
+const DayPass = ({ icon, hasIconRight, title, onClick, primary }) => {
+  return (
+    <button
+      className={classNames([
+        styles.button,
+        primary ? styles.primaryBG : styles.normalBG,
+        hasIconRight && styles.reverse,
+      ])}
+      onClick={onClick}
+      >
+      {!!icon && (
+        <span
+          className={classNames([
+            styles.icon,
+            hasIconRight && styles.iconRight,
+
+          ])}
+        >
+          {icon}
+        </span>
+      )}
+      <span>{title}</span>
+    </button>
+  );
+};
+
+export default DayPass;
